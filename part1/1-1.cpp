025c66078500b9775c4d8ad4c039766df4f9a756
jeongminlee <chopin.vii@gmail.com>
2023-01-08 22:43:39 +0900

    first commit

---
+++ part1/1-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, b, c, d, e;
+  cout << "input first int: ";
+  cin >> a;
+  cout << "input second int: ";
+  cin >> b;
+  cout << "input third int: ";
+  cin >> c;
+  cout << "input forth int: ";
+  cin >> d;
+  cout << "input fifth int: ";
+  cin >> e;
+  cout << a + b + c + d + e << endl;
+  return 0;
+}
