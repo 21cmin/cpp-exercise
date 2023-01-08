9cc6b5ecf73abbe083956d61d04b4d992289acf4
jeongminlee <chopin.vii@gmail.com>
2023-01-08 23:21:07 +0900

    second commit

---
+++ part1/1-4/func.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "func.h"
+
+using namespace std;
+
+void BestComImpl::SimpleFunc(void)
+{
+  cout << "func of bestcom" << endl;
+}
+
+void ProgComImpl::SimpleFunc(void)
+{
+  cout << "func of progcom" << endl;
+}
